b65ddd8920ee15cbb791826db6c3f9b5121d3fb5
Muhammad Ibrahim Mubashir <160342010+MuhammadIbrahimMubashir@users.noreply.github.com>
2024-05-19 22:05:05 +0500

    Add files via upload

---
+++ Day 34/34.1.js
@@ -0,0 +1,4 @@
+// Finds the square root of 144 using Math.sqrt()
+var squareRoot = Math.sqrt(144);
+console.log(squareRoot); // Outputs: 12
+// This line effectively calculates and shows the square root of 144.
